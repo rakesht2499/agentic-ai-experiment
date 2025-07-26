bfc8362c2554a60387e96778564f436408b92e6b
rakesht2499 <rakesht2499@gmail.com>
2025-07-27 01:26:10 +0530

    ok comitted by suhana

--- main.py
+++ main.py
@@ -12,44 +12,48 @@ if __name__ == "__main__":
     project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
     location = os.getenv("GOOGLE_CLOUD_LOCATION")
     bucket = os.getenv("GOOGLE_CLOUD_STAGING_BUCKET")
-
-    agent = agent_engines.get("projects/rag-engine-vertex-ai-project/locations/us-central1/reasoningEngines/8302482670080753664")
-    print(agent.operation_schemas())
-    print(agent.stream_query(input="I am a teacher, can you explain heredity from class 10 scince?"))
+    #
+    # agent = agent_engines.get("projects/rag-engine-vertex-ai-project/locations/us-central1/reasoningEngines/8302482670080753664")
+    # print(agent.operation_schemas())
+    # print(agent.stream_query(input="I am a teacher, can you explain heredity from class 10 scince?"))
     #
     # try:
     #     nltk.data.find("tokenizers/punkt")
     # except LookupError:
     #     nltk.download("punkt")
     #
-    # vertexai.init(
-    #     project=project_id,
-    #     location=location,
-    #     staging_bucket=bucket,
-    # )
-    #
-    # remote_app = agent_engines.create(
-    #     display_name="Shahayak 2.0 - test 1",
-    #     agent_engine=root_agent,
-    #     requirements=[
-    #         "google-cloud-aiplatform[adk,agent_engines]==1.104.0",
-    #         "google-adk==1.7.0",
-    #         "python-dotenv==1.1.0",
-    #         "requests>=2.32.4",
-    #         "pydantic==2.11.3",
-    #         "absl-py==2.1.0",
-    #         "cloudpickle==3.0.0",
-    #         "deprecated>=1.2.14",
-    #     ],
-    #     extra_packages=[
-    #         "./request_processor_agent",
-    #         "./answer_orchastrator_agent",
-    #         "./diagram_generating_agent",
-    #         "./image_generating_agent",
-    #         "./lesson_planning_agent",
-    #         "./quiz_generating_agent_new",
-    #         "./common_agents",
-    #         "./syllabus_planning_agent",
-    #         "./models"
-    #     ],
-    # )
+    vertexai.init(
+        project=project_id,
+        location=location,
+        staging_bucket=bucket,
+    )
+
+    remote_app = agent_engines.create(
+        display_name="Shahayak 2.0",
+        agent_engine=root_agent,
+        requirements=[
+            "google-cloud-aiplatform[adk,agent_engines]==1.104.0",
+            "google-adk==1.7.0",
+            "python-dotenv==1.1.0",
+            "requests>=2.32.4",
+            "pydantic==2.11.3",
+            "absl-py==2.1.0",
+            "cloudpickle==3.0.0",
+            "deprecated>=1.2.14",
+            "networkx >= 3.0",
+            "Pillow >= 10.0.0"
+        ],
+        extra_packages=[
+            "./answer_orchastrator_agent",
+            "./common_agents",
+            "./diagram_generating_agent",
+            "./image_generating_agent",
+            "./kg_quiz_generating_agent_new",
+            "./knowledge_graph",
+            "./lesson_planning_agent",
+            "./models",
+            "./quiz_generating_agent_new",
+            "./request_processor_agent",
+            "./syllabus_planning_agent",
+        ],
+    )
--- request_processor_agent/agent.py
+++ request_processor_agent/agent.py
@@ -2,7 +2,7 @@ from google.adk.agents import LlmAgent
 from google.adk.tools import agent_tool
 
 from quiz_generating_agent_new.agent import root_agent as exam_generating_agent
-from cursor_diagram_generator.agent import diagram_generating_agent
+from diagram_generating_agent.agent import diagram_generating_agent
 from image_generating_agent.agent import image_generating_agent
 from lesson_planning_agent.agent import root_agent as lesson_planning_agent
 from answer_orchastrator_agent.agent import answer_orchestrator_agent
